7eb4a586bf08d87fa7c6bab4a4444d606c5e80b3
sairim <spiri@protonmail.com>
2019-04-14 21:34:30 +0300

    connect register front end with backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <div className="container">
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </div>
+          <Footer />
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- validation/register.js
+++ validation/register.js
@@ -9,12 +9,12 @@ module.exports = function valdiateRegisterInput(data) {
     data.password = !isEmpty(data.password) ? data.password : '';
     data.password2 = !isEmpty(data.password2) ? data.password2 : '';
 
-    if (Validator.isEmpty(data.name)) {
-        errors.name = 'Name field is required.'
-    }
-
     if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
         errors.name = 'Name must be between 2 and 30 characters';
+    }
+
+    if (Validator.isEmpty(data.name)) {
+        errors.name = 'Name field is required.'
     }
 
     if (Validator.isEmpty(data.email)) {
@@ -30,7 +30,7 @@ module.exports = function valdiateRegisterInput(data) {
     }
 
     if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
-        errors.passowrd = 'Password must be at least six characters'
+        errors.password = 'Password must be at least six characters'
     }
 
     if (Validator.isEmpty(data.password2)) {
